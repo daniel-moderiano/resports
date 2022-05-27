b6af161710bed31d20dbffff57e524f144f9ab34
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-05-27 17:50:09 +0930

    Use GAPI context in useGapiClient hook to avoid duplicate initialisation of GAPI client

--- hooks/useGapiClient.ts
+++ hooks/useGapiClient.ts
@@ -7,19 +7,18 @@ export const useGapiClient = () => {
 
   useEffect(() => {
     // Initialises an instance of the GAPI client using the provided API key. If OAuth is required, these credentials may be provided here.
-    const initialiseGapi = async () => {
-      await gapi.client.init({
+    const initialiseGapi = () => {
+      gapi.client.init({
         'apiKey': process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
-      });
-
-      console.log('GAPI initialised');
+      })
+        .then(() => console.log('GAPI initialised'))
+        .catch(err => console.log(err))
     };
 
     // Check that the GAPI script has loaded and is available. Because of the server-side rendering, theoretically there shouldn't be a case where this hook is called before the GAPI script has initialised?
     if (typeof gapi === 'undefined') {
-      console.log('GAPI script unavailable');
+      console.error('GAPI script unavailable');
     } else {
-      console.log('GAPI script loaded');
       if (!gapiClientReady) {   // GAPI client has not previously been initialised
         gapi.load('client', initialiseGapi);
         setGapiClientReady(true);
