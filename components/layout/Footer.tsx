5e8faa0a94672ad1b4dacee716b52a8a9c179e25
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-06-24 15:24:05 +0930

    Fix layout height not spanning 100%vh

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import styles from '../../styles/componentStyles/Footer.module.css'
 const Footer = () => {
   return (
     <footer role="contentinfo" className={styles.footer}>
-      <p>&copy; Resports 2022</p>
+      <p>&copy; DM 2022</p>
     </footer>
   )
 }
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ import styles from '../../styles/componentStyles/Header.module.css'
 const Header = () => {
   return (
     <header role="banner" className={styles.header}>
-      <h1 className={styles.headerTitle}>Resports</h1>
+      <h1 className={styles.headerTitle}>Logo here</h1>
       <div>Searchbar</div>
       <Nav />
     </header>
