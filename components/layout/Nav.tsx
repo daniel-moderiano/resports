fcea3cf6fe6a8414a348e5cb469df64073aecc53
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-06-24 10:17:10 +0930

    Add Nav.module.css

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -1,15 +1,16 @@
 // * If anchor tags are to be placed in the list items, these should become role="menuitem" and the <li> tags should have role="none"
 
 // The Nav is also likely to contain a search bar and a logo, that links back to the home page
+import styles from '../../styles/Nav.module.css'
 
 const Nav = () => {
   return (
-    <nav role="navigation">
-      <ul role="menubar">
-        <li role="menuitem">Home</li>
-        <li role="menuitem">Sign up</li>
-        <li role="menuitem">Log in</li>
-        <li role="menuitem">Log out</li>
+    <nav role="navigation" className="nav">
+      <ul role="menubar" className={styles.navList}>
+        <li role="menuitem" className={styles.navListItem}>Home</li>
+        <li role="menuitem" className={styles.navListItem}>Sign up</li>
+        <li role="menuitem" className={styles.navListItem}>Log in</li>
+        <li role="menuitem" className={styles.navListItem}>Log out</li>
       </ul>
     </nav>
   )
