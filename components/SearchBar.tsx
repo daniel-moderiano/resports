3bd3d09c7333e399813887aeec469e9967fe5017
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-06-25 13:15:41 +0930

    Replace search text with search icon

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useRouter } from 'next/router';
 import styles from '../styles/componentStyles/SearchBar.module.css'
 
-// TODO: Add search icon instead of text 'search'
 // TODO: Add custom disabled styles that replicate normal btn, but with 'stop sign' cursor
 
 // This component is expected to live in the header and remain visible from all pages of the application
@@ -20,7 +19,7 @@ const SearchBar = () => {
   }
 
   return (
-    <div>
+    <div className={styles.searchBar}>
       <label htmlFor="search" className={styles.label}>Search</label>
       <input
         type="text"
@@ -33,9 +32,11 @@ const SearchBar = () => {
         autoComplete="false"
       />
       <button
+        className={styles.button}
         onClick={handleClick}
         disabled={searchQuery.trim() === ''}>
-        Search
+        {/* Consider abstracting this to an icon component */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" /></svg>
       </button>
     </div>
   )
