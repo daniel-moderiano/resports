2d8f895fe6dab46b3e0c2776b0b3ffd6503552a8
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-06-26 17:06:51 +0930

    Adjust GAPI query to perform Search: list query for channels

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,17 +4,47 @@ import { useGapiContext } from '../context/GapiContext';
 import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 
+// Individual search result items
 interface SearchResult {
+  kind: string;
+  etag: string;
+  id: {
+    kind: string;
+    videoId?: string;
+    channelId?: string;
+    playlistId?: string;
+  },
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      [key: string]: {
+        url: string,
+        width?: number;
+        height?: number;
+      }
+    },
+    channelTitle: string,
+    liveBroadcastContent: string
+  }
+}
+
+// Returned by the Search: list YouTube API method
+interface SearchListResponse {
   kind: string;
   etag: string;
   nextPageToken?: string;
   prevPageToken?: string;
+  regionCode: string;
   pageInfo: {
     totalResults: number;
     resultsPerPage: number;
   }
-  items: [];
+  items: SearchResult[];
 }
+
 
 // Use this to ensure searchQueries provided via the URL are in the correct format for API calls
 // Exported for testing purposes
@@ -25,27 +55,32 @@ export const isValidQuery = (query: ParsedUrlQuery) => {
   return false;
 }
 
-
+// TODO: Consider a toggle between channels/playlists/videos, or a seies or checkboxes
 // TODO: Add GAPI client search request using React Query. Enable on isValidQuery check!
 
 const Search = () => {
   // Extract the query params in object form
   const router = useRouter();
   const UrlQuery = router.query;
+  // Set here to avoid constant API calls burning quota
+  // const UrlQuery = {};
+
 
   const { gapiClientReady } = useGapiContext();
 
   const { isLoading, isError, data, error, isIdle } = useQuery(['request'], async () => {
     // GAPI client will throw it's own error if there is a problem with the request, there is no need for a specific try/catch here
     const response = await gapi.client.request({
-      'path': 'https://youtube.googleapis.com/youtube/v3/channels',
+      'path': 'https://youtube.googleapis.com/youtube/v3/search',
       params: {
-        id: 'UCj1J3QuIftjOq9iv_rr7Egw',
-        part: 'contentDetails, snippet'
+        part: 'snippet',    // the type/nature of data returned
+        q: UrlQuery.searchQuery,    //search query
+        type: 'channel',   // restrict to channels only
+        maxResults: 25    // max number of results per page
       }
     });
 
-    return response
+    return response.result as SearchListResponse;
   }, {
     enabled: (gapiClientReady && isValidQuery(UrlQuery)),
   });
@@ -74,6 +109,16 @@ const Search = () => {
   return (
     <div>
       <h2 className="title">You searched for {isValidQuery(UrlQuery) ? UrlQuery.searchQuery : 'an invalid result'}</h2>
+      {data && (
+        <>
+          {data.items.map((item) => (
+            <div key={item.etag}>
+              <h3>{item.snippet.channelTitle}</h3>
+              <p>{item.snippet.description}</p>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   )
 }
