c154f7618a3d3a64ef6e09680db32dcfeaf2f39a
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-06-28 20:15:05 +0930

    Create separate tab components for search pages of each platform

---
+++ components/TwitchSearchTab.tsx
@@ -0,0 +1,12 @@
+
+interface TwitchSearchTabProps {
+  searchQuery: string;
+}
+
+const TwitchSearchTab = ({ searchQuery }: TwitchSearchTabProps) => {
+  return (
+    <div>TwitchSearchTab</div>
+  )
+}
+
+export default TwitchSearchTab
---
+++ components/YouTubeSearchTab.tsx
@@ -0,0 +1,12 @@
+
+interface YouTubeSearchTabProps {
+  searchQuery: string;
+}
+
+const YouTubeSearchTab = ({ searchQuery }: YouTubeSearchTabProps) => {
+  return (
+    <div>YouTubeSearchTab</div>
+  )
+}
+
+export default YouTubeSearchTab
