d13d3ff03dc00f4996f9d4f70817a0f7ffe8c0fe
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-07-04 11:42:09 +0930

    Remove unused Playwright test library

--- playwright.config.ts
+++
@@ -1,7 +0,0 @@
-// playwright.config.ts
-import { PlaywrightTestConfig } from '@playwright/test';
-
-const config: PlaywrightTestConfig = {
-  testMatch: /.e2e\.*(test|spec)\.(js|ts|mjs)/
-};
-export default config;
