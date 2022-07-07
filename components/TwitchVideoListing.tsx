dad1fc4230fe6affbb7e908da71c822533cd6bc2
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-07-07 10:15:52 +0930

    Add JS library to convert datetimes to 'x time ago...' format

--- components/TwitchVideoListing.tsx
+++ components/TwitchVideoListing.tsx
@@ -1,6 +1,10 @@
 import { HelixVideo } from "@twurple/api/lib";
 import Image from 'next/image';
-import { convertTwitchVideoDuration } from '../helpers/videoDurationConversion'
+import { convertTwitchVideoDuration } from '../helpers/videoDurationConversion';
+import TimeAgo from 'javascript-time-ago';
+
+// Create datetime formatter (English language). This is used to convert JS Dates to a 'x time ago..' format
+const timeAgo = new TimeAgo('en-AU')
 
 interface TwitchVideoListingProps {
   videoData: HelixVideo
@@ -13,7 +17,7 @@ const TwitchVideoListing = ({ videoData }: TwitchVideoListingProps) => {
       <p>{videoData.description}</p>
       <p>{convertTwitchVideoDuration(videoData.duration)}</p>
       <Image src={videoData.getThumbnailUrl(169, 100)} height={100} width={169} layout="fixed" alt="Video thumbnail" />
-      <p>{videoData.creationDate.toLocaleString()}</p>
+      <p>{timeAgo.format(videoData.creationDate)}</p>
       <p>{videoData.userDisplayName}</p>
     </div>
   )
