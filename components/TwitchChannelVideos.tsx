a9ecd9c3e1c71e3fa2f65994fb996dcac33fc8e4
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-07-12 12:09:01 +0930

    Refactor to include videoType in the general video filters state

--- components/TwitchChannelVideos.tsx
+++ components/TwitchChannelVideos.tsx
@@ -9,48 +9,34 @@ interface TwitchChannelVideosProps {
   userId: string;
 }
 
+interface VideoFilters {
+  dateFilter: Date | null;
+  durationFilter: number | null;
+  keywordFilter: string | null;
+  videoType: HelixVideoType | undefined | 'all';
+}
+
 // Make API call here to fetch videos using channel/user ID
 // * Use the archive video type filter to get past broadcasts!!
 
 const TwitchChannelVideos = ({ userId }: TwitchChannelVideosProps) => {
-  const [videoType, setVideoType] = React.useState<HelixVideoType | undefined | 'all'>('archive');
-  const { isError, isLoading, data } = useGetTwitchVideos(userId, videoType);
+  const [filters, setFilters] = React.useState<VideoFilters>({
+    dateFilter: null,
+    durationFilter: null,
+    keywordFilter: null,
+    videoType: 'archive'
+  });
+
+  const { isError, isLoading, data } = useGetTwitchVideos(userId, filters.videoType);
   const [filteredVideos, setFilteredVideos] = React.useState<HelixVideo[] | undefined | null>(null);
-
-  const [dateFilter, setDateFilter] = React.useState<Date | null>(null);
-  const [durationFilter, setDurationFilter] = React.useState<number | null>(null);
-  const [keywordFilter, setKeywordFilter] = React.useState<string | null>(null);
-
 
   const handleOptionSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     // This typecasting is required, as you cannot simply assign the 'string' value type to the videoType state
-    setVideoType(e.target.value as HelixVideoType | 'all');
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+        videoType: e.target.value as HelixVideoType | 'all'
+    }));
   }
-
-  // The use of an effect here ensures that the data set is not only filtered before initial rendering of data, but is open to modification after the data has been rendered.
-  React.useEffect(() => {
-    if (data && filteredVideos) {   // cannot filter data set until it is available
-
-      // Set initial data set to filteredVideos state. This only runs on initial render
-      if (filteredVideos === null) {
-        setFilteredVideos(data)
-      }
-
-      // Re-filter videos
-      if (dateFilter) {
-        setFilteredVideos(filterByDate(filteredVideos, dateFilter));
-      }
-
-      if (durationFilter) {
-        setFilteredVideos(filterByDuration(filteredVideos, durationFilter));
-      }
-
-      if (keywordFilter) {
-        setFilteredVideos(filterByKeyword(filteredVideos, keywordFilter));
-      }
-    }
-  }, [data, dateFilter, durationFilter, keywordFilter]);
-
 
   return (
     <section>
@@ -60,7 +46,7 @@ const TwitchChannelVideos = ({ userId }: TwitchChannelVideosProps) => {
       {isError && (<div>An error has occurred</div>)}
 
       <label htmlFor="videoType">Video type</label>
-      <select defaultValue={videoType} onChange={handleOptionSelect} name="videoType" id="videoType">
+      <select defaultValue={filters.videoType} onChange={handleOptionSelect} name="videoType" id="videoType">
         <option value="all" data-testid="allOption">All videos</option>
         <option value="archive" data-testid="broadcastOption">Broadcasts</option>
       </select>
---
+++ components/TwitchFilterMenu.tsx
@@ -0,0 +1,26 @@
+
+const TwitchFilterMenu = () => {
+
+
+  return (
+    <div>
+      {/*Expand and collapse the filter menu*/}
+      <button>Filters</button>
+
+      <div>
+        <h4>Upload date</h4>
+      </div>
+      <div>
+        <h4>Duration</h4>
+      </div>
+      <div>
+        <h4>Keyword Search</h4>
+      </div>
+      <div>
+        <h4>Type</h4>
+      </div>
+    </div>
+  );
+};
+
+export default TwitchFilterMenu;
