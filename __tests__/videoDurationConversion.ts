da26e057bb394cb77b77b506f2e4b64456b92bf9
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-07-16 15:40:15 +0930

    Create YT video duration converter test suite

--- __tests__/videoDurationConversion.ts
+++ __tests__/videoDurationConversion.ts
@@ -29,3 +29,33 @@ describe('Twitch video duration conversion', () => {
     expect(convertTwitchVideoDuration('11m1s')).toBe('11:01')
   });
 });
+
+describe('YouTube video duration conversion', () => {
+  it('Converts seconds-only input', () => {
+    expect(convertTwitchVideoDuration('P44S')).toBe('0:44')
+  });
+
+  it('Converts minutes/seconds-only input', () => {
+    expect(convertTwitchVideoDuration('P3M21S')).toBe('3:21')
+  });
+
+  it('Converts full hours/minutes/seconds input', () => {
+    expect(convertTwitchVideoDuration('P11H10M12S')).toBe('11:10:12')
+  });
+
+  it('Handles single digit hours correctly', () => {
+    expect(convertTwitchVideoDuration('P5H10M10S')).toBe('5:10:10')
+  });
+
+  it('Handles single digit minutes correctly', () => {
+    expect(convertTwitchVideoDuration('P10H5M12S')).toBe('10:05:12')
+  });
+
+  it('Handles single digit seconds correctly', () => {
+    expect(convertTwitchVideoDuration('P11H10M0S')).toBe('11:10:00')
+  });
+
+  it('Handles single digit seconds correctly (leading zeros)', () => {
+    expect(convertTwitchVideoDuration('P11M1S')).toBe('11:01')
+  });
+});
--- helpers/videoDurationConversion.ts
+++ helpers/videoDurationConversion.ts
@@ -3,7 +3,49 @@ export const convertTwitchVideoDuration = (duration: string) => {
   let convertedDuration = '';
 
   // Splitting at the hours allows use to handle all the conditionals easily from the top-down
-  // It also correctlyhandles input no matter the duration, whereas using a length conditional would vary for 0 mins/secs
+  // It also correctly handles input no matter the duration, whereas using a length conditional would vary for 0 mins/secs
+  const hoursSplit = duration.split('h');
+
+  if (hoursSplit.length === 1) {    // duration is either minutes/seconds or seconds only
+    const minutesSplit = duration.split('m');
+
+    if (minutesSplit.length > 1) {    // duration is minutes/seconds
+      // Handle the minutes component (add extra zeros for single digit mins)
+      convertedDuration += `${minutesSplit[0]}:`;
+
+      // Handle the seconds component
+      const secondsSplit = minutesSplit[1].split('s');
+      convertedDuration += secondsSplit[0].length > 1 ? `${secondsSplit[0]}` : `0${secondsSplit[0]}`
+    } else {    // duration is seconds only
+      // Handle seconds component
+      convertedDuration += `0:${duration.split('s')[0]}`;
+    }
+  }
+
+  // duration is > 1 hour (i.e. contains hours/minutes/seconds components)
+  if (hoursSplit.length > 1) {
+    // Handle hours component
+    convertedDuration += `${hoursSplit[0]}:`;
+
+    // Handle minutes component (add extra zeros for single digit mins)
+    const minutesSplit = hoursSplit[1].split('m');
+    convertedDuration += minutesSplit[0].length > 1 ? `${minutesSplit[0]}:` : `0${minutesSplit[0]}:`
+
+    // Handle the seconds component  (add extra zeros for single digit secs)
+    const secondsSplit = minutesSplit[1].split('s');
+    convertedDuration += secondsSplit[0].length > 1 ? `${secondsSplit[0]}` : `0${secondsSplit[0]}`
+  }
+
+  return convertedDuration;
+};
+
+
+// YouTube-specific video duration converter. Expects string input in ISO 8601 duration format, and should yield of the form HH:MM:SS
+export const convertYouTubeVideoDuration = (duration: string) => {
+  let convertedDuration = '';
+
+  // Splitting at the hours allows use to handle all the conditionals easily from the top-down
+  // It also correctly handles input no matter the duration, whereas using a length conditional would vary for 0 mins/secs
   const hoursSplit = duration.split('h');
 
   if (hoursSplit.length === 1) {    // duration is either minutes/seconds or seconds only
