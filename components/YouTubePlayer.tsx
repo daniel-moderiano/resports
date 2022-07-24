b7b984337c9392bbf4cb1964e96909efaf0cc6aa
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-07-24 18:35:09 +0930

    Create theater styling for iframe, with toggle button

--- components/YouTubePlayer.tsx
+++ components/YouTubePlayer.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import styles from '../styles/componentStyles/YouTubePlayer.module.css'
 
 // The useYouTubeIframe hook will add the YouTube iframe to this div with id="player"
 const YouTubePlayer = () => {
+
+  const [theaterMode, setTheaterMode] = useState(false);
+
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${theaterMode ? styles.wrapperTheater : styles.wrapperNormal}`}>
       <div id="player"></div>
+      <button onClick={() => { setTheaterMode((prevState) => !prevState) }} className={styles.toggle}>Toggle theater mode</button>
     </div>
   )
 }
