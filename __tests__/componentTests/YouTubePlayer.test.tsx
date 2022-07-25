15b7bdee90668d07e3c38d3ae8caefbb3a1dcecd
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-07-25 18:20:54 +0930

    Add testing for theater mode toggle

--- __tests__/componentTests/YouTubePlayer.test.tsx
+++ __tests__/componentTests/YouTubePlayer.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 import YouTubePlayer from '../../components/YouTubePlayer';
 
 describe('YouTube player styling and toggles', () => {
@@ -6,5 +7,31 @@ describe('YouTube player styling and toggles', () => {
     render(<YouTubePlayer />)
     const wrapper = screen.getByTestId('wrapper');
     expect(wrapper).toHaveClass('wrapperNormal');
-  })
-})
+    expect(wrapper).not.toHaveClass('wrapperTheater');
+  });
+
+  it('Toggles theater mode on button click', async () => {
+    render(<YouTubePlayer />)
+    const wrapper = screen.getByTestId('wrapper');
+    const btn = screen.getByRole('button', { name: /theater/i })
+
+    // Switch to theater mode
+    await userEvent.click(btn);
+
+    expect(wrapper).not.toHaveClass('wrapperNormal');
+    expect(wrapper).toHaveClass('wrapperTheater');
+  });
+
+  it('Toggles back to normal mode on subsequent button click', async () => {
+    render(<YouTubePlayer />)
+    const wrapper = screen.getByTestId('wrapper');
+    const btn = screen.getByRole('button', { name: /theater/i })
+
+    // Switch to theater mode, then switch back
+    await userEvent.click(btn);
+    await userEvent.click(btn);
+
+    expect(wrapper).toHaveClass('wrapperNormal');
+    expect(wrapper).not.toHaveClass('wrapperTheater');
+  });
+});
