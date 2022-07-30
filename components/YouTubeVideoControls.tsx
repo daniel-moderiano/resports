0ba35fc8fe472987ac19e2dcb5fa6551e306dc24
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-07-30 21:49:02 +0930

    Add new theater icon

--- components/YouTubeVideoControls.tsx
+++ components/YouTubeVideoControls.tsx
@@ -96,8 +96,8 @@ const YouTubeVideoControls = ({
         </button>
 
         {/* Theater btn */}
-        <button className={styles.controlsBtn} onClick={toggleTheater}>
-          <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" viewBox="0 0 24 24" width="30px" fill="#FFFFFF"><g><rect fill="none" height="32" width="32" /></g><g><g><g><path d="M19,7H5C3.9,7,3,7.9,3,9v6c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9C21,7.9,20.1,7,19,7z M19,15H5V9h14V15z" /></g></g></g></svg>
+        <button className={`${styles.controlsBtn} ${styles.theaterBtn}`} onClick={toggleTheater}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 138.7 90.93" width="24px"><rect x="3.5" y="3.5" width="131.7" height="83.93" fill="none" stroke="#FFFFFF" strokeWidth="24px" /></svg>
         </button>
 
         {/* Enter fullscreen btn */}
