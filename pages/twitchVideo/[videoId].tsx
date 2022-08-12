e2e7fd1985d87f51fa5d194146f65c7cfba354ea
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-08-12 14:55:49 +0930

    Create Twitch Video page and add link to all video listings

---
+++ pages/twitchVideo/[videoId].tsx
@@ -0,0 +1,29 @@
+// The video/watch page that houses an embedded Twitch iframe/player
+import { GetServerSideProps } from "next";
+import { sanitiseVideoQuery } from "../../helpers/queryHandling";
+
+interface TwitchVideoProps {
+  videoId: string;
+}
+
+// This server side props function ensures the dynamic route param is made available at component render time so that it can be passed safely and directly to the TwitchVideo component hook. Using router.query in component causes videoId to be undefined on initial render.
+/* eslint-disable-next-line */
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const videoId = sanitiseVideoQuery(context.query);
+
+  // Pass data to the page via props
+  return { props: { videoId } }
+}
+
+
+const TwitchVideo = ({ videoId }: TwitchVideoProps) => {
+  return (
+    <div>
+      <p>{videoId}</p>
+      Hello Sarah
+    </div>
+  );
+};
+
+export default TwitchVideo;
+
